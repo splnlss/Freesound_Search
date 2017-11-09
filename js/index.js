bb25d58be9a00687af4453f754c3315875500896
splnlss <sans.spleen@gmail.com>
2017-11-09 12:06:05 -0500

    added sounds on rollover

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const FREESOUND_SEARCH_URL = 'https://freesound.org/apiv2/search/text/'
 const FREESOUND_SOUND_URL = 'https://freesound.org/apiv2/sounds/'
 //const FREESOUND_SIMILAR_URL = /apiv2/sounds/<sound_id>/similar/
+const colorSVG = ["#F44336", "#E91E63", "#9C27B0", "#673AB7", "#2196F3", "#00BCD4", "#009688", "#4CAF50", "#8BC34A", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722"]
 const soundsGLOBAL = []
 let soundCountGLOBAL = 0
 
@@ -31,7 +32,10 @@ const freeSoundPreview = (sound) =>{
       token: 'arDESEy8t1jg4YeEM3tUntX3MXMeuZBlxEQu9evS',
     },
     success: (data) => {
-      sound.previewURL = data.previews['preview-hq-mp3']
+      sound.previewURL = data.previews['preview-hq-mp3'] //no longer necessary
+      sound.previewMP3 = new Howl({
+                src: [data.previews['preview-hq-mp3'], data.previews['preview-hq-ogg']]
+                })
       soundCountGLOBAL++
       if (soundCountGLOBAL >= soundsGLOBAL.length) {
         createSVG(soundsGLOBAL)
@@ -42,9 +46,10 @@ const freeSoundPreview = (sound) =>{
   $.ajax(settings)
 }
 
-const freeSoundSimilar = (event) =>{
-  console.log(event.currentTarget)
-  const id = $(event.currentTarget).data('id')
+const freeSoundSimilar = (clickedSoundID) =>{
+  // console.log(event.currentTarget)
+  // const id = $(event.currentTarget).data('id')
+  const id = clickedSoundID
   const settings = {
     url: `${FREESOUND_SOUND_URL}${id}/similar/`,
     type: `GET`,
@@ -60,7 +65,6 @@ const freeSoundSimilar = (event) =>{
   }
   $.ajax(settings)
 }
-const colorSVG = ["#F44336", "#E91E63", "#9C27B0", "#673AB7", "#2196F3", "#00BCD4", "#009688", "#4CAF50", "#8BC34A", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722"]
 
 const importData = (data)=>{
   console.log(data)
@@ -96,12 +100,7 @@ const svgCanvas = d3.select('main')
                 .attr('width', 800)
                 .attr('height', 600)
 
-const links = svgCanvas.selectAll('circle')
-                  .data(data)
-                  .append("a")
-                      .attr("xlink:href", function (d){ return d.previewURL})
-
-const circleAttributes = links
+const circleAttributes = svgCanvas.selectAll('circle')
                   .data(data)
                   .enter()
                   .append('circle')
@@ -112,20 +111,18 @@ const circleAttributes = links
                       .attr("r", "40")
                       .attr("id",  function (d,i) { return `button_${i}`; })
                       .style("fill", function (d){return d.color})
-                      .on("mouseover", function() {
-                          console.log("mouse over")
+                      .on("mouseover", function(d) {
                           d3.select(this).style("opacity", .2)
+                          d.previewMP3.play()
                         })
-                      .on('mouseleave', function(){
-                          console.log("mouse leave")
+                      .on('mouseleave', function(d){
                           d3.select(this).style("opacity", 1)
-                      .on('click')
-// const circleFill = circleAttributes
-//                   .data(freesoundCircles)
-//                   //.enter()
-//                   .append('circle')
-//                       .style("fill", "blue")
-    })
+                          d.previewMP3.stop()
+                        })
+                      .on('click', function(d, i) {
+                          console.log('mouse click')
+                          freeSoundSimilar(d.id)
+                        })
 }
 
 const setupUIHandlers = () => {
