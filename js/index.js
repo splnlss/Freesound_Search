fd0b62164da16d0962becc05bcef05d368fb938a
splnlss <sans.spleen@gmail.com>
2017-11-15 10:11:10 -0500

    spoke and wheel running

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 const FREESOUND_SEARCH_URL = 'https://freesound.org/apiv2/search/text/'
 const FREESOUND_SOUND_URL = 'https://freesound.org/apiv2/sounds/'
-//const TOKEN = '0I1HhPkTR6FNyhlsfoq4FQAAGob5bgvl6LAlO7A3'
-const TOKEN = 'arDESEy8t1jg4YeEM3tUntX3MXMeuZBlxEQu9evS'
+const TOKEN = '0I1HhPkTR6FNyhlsfoq4FQAAGob5bgvl6LAlO7A3'
+//const TOKEN = 'arDESEy8t1jg4YeEM3tUntX3MXMeuZBlxEQu9evS'
+//const TOKEN = 'Y65kZl2GTZuwcJ9YXtStETJ42ExnHOiPvEyd5Sxd'
 //const FREESOUND_SIMILAR_URL = /apiv2/sounds/<sound_id>/similar/
 const colorSVG = ["#F44336", "#E91E63", "#9C27B0", "#673AB7", "#3F51B5", "#2196F3", "#00BCD4", "#009688", "#4CAF50", "#8BC34A", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722"]
 const soundsGLOBAL = []
 let soundCountGLOBAL = 0
-const totalResults = 8
-const width = 800
-const height = 600
+const totalResults = 8  // make upper case to designate global
+const WIDTH = 800
+const HEIGHT = 600
 
 const freeSoundAPI = (search) =>{
   const settings = {
@@ -23,11 +24,11 @@ const freeSoundAPI = (search) =>{
     },
     success: (data) => { importData(data.results) },
     failure: (error) => { console.log(`error: ${error}`) }
-    }
+  }
   $.ajax(settings)
 }
 
-const freeSoundPreview = (sound, clickedSoundID) =>{
+const freeSoundPreview = (sound, clickedSoundID, clickedSoundIndex) =>{
   const settings = {
     url: `${FREESOUND_SOUND_URL}${sound.id}`,
     type: `GET`,
@@ -36,22 +37,16 @@ const freeSoundPreview = (sound, clickedSoundID) =>{
       format: 'jsonp',
       token: TOKEN,
     },
-    success: (data) => { // should all below be moved to a sepparate function???
+    success: (data) => {
+      sound.previewMP3 = new Howl({
+        src: [data.previews['preview-hq-mp3'], data.previews['preview-hq-ogg']]
+      })
       soundCountGLOBAL++
-      if (soundCountGLOBAL >= soundsGLOBAL.length) {
-        console.log(`soundsGLOBAL.length = ${soundsGLOBAL.length}`)
-        if((soundsGLOBAL.length) == 1){               // is this not writing over original??
-          sound.previewURL = data.previews['preview-hq-mp3'] //no longer necessary
-          sound.previewMP3 = new Howl({
-                    src: [data.previews['preview-hq-mp3'], data.previews['preview-hq-ogg']]
-                    })
-          createSVG(soundsGLOBAL[(soundsGLOBAL.length-1)], clickedSoundID)
+      if (soundCountGLOBAL >= soundsGLOBAL.length) { // this only truly works the first time
+        if(clickedSoundID){
+          onClickSVG((soundsGLOBAL[clickedSoundIndex].similar), clickedSoundID)
         }else{
-          sound.previewURL = data.previews['preview-hq-mp3'] //no longer necessary
-          sound.previewMP3 = new Howl({
-                    src: [data.previews['preview-hq-mp3'], data.previews['preview-hq-ogg']]
-                    })
-          onClickSVG(soundsGLOBAL[(soundsGLOBAL.length-1)], clickedSoundID)
+          createSVG(soundsGLOBAL)
         }
       }
     },
@@ -71,103 +66,96 @@ const freeSoundSimilar = (clickedSoundID, clickedSoundIndex) =>{
       token: TOKEN,
     },
     success: (data) => {
-      importData(data.results, clickedSoundID)
-      console.log(clickedSoundID)
+      importData(data.results, clickedSoundID, clickedSoundIndex)
     },
     failure: (error) => { console.log(`error: ${error}`) }
   }
   $.ajax(settings)
 }
 
-const importData = (data, clickedSoundID)=>{
+const importData = (data, clickedSoundID, clickedSoundIndex)=>{
   let count = 0;
   let resultArray = []
   const colorArray = []
   const randomColor = (max) =>{
-        const randomOutput = Math.round(Math.random(max) *10)
-        //console.log(colorArray.includes(randomOuput))
-        // while((colorArray.includes(randomOuput)) {
-        //   randomOutput = Math.round(Math.random(max) *10)
-        // }else{
-          //colorArray.push(randomOuput)
-          return randomOutput
-        }
-  if (data){
-    if(clickedSoundID){
-                  //don't use array, use objects!!
-      data.forEach((item, index) => {
-        const soundSimilar = {
-          name: item.name,
-          id: item.id,
-          color: colorSVG[(randomColor(index))]
-        }
-      resultArray[index] = soundSimilar
-      freeSoundPreview(soundSimilar, clickedSoundID)
-      //onClickSVG(soundSimilar, clickedSoundIndex)
-      })
-      soundsGLOBAL.push(resultArray)
-      console.log(soundsGLOBAL[(soundsGLOBAL.length-1)])
-
-    }else{
-      clickedSoundID = 100001
-      data.forEach((item, index) => {
-          const sound = {
-            name: item.name,
-            id: item.id,
-            color: colorSVG[index]
-          }
-        resultArray[index] = sound //naming of clickedSoundIndex not working...
-        freeSoundPreview(sound, clickedSoundID) //set clickedSoundIndex after sending it to 1st click
-      })
-      soundsGLOBAL.push(resultArray)
-    }
-
-  }else{
-    $('#results').html('Zero Results')
+    const randomOutput = Math.round(Math.random(max) *10)
+    //console.log(colorArray.includes(randomOuput))
+    // while((colorArray.includes(randomOuput)) {
+    //   randomOutput = Math.round(Math.random(max) *10)
+    // }else{
+    //colorArray.push(randomOuput)
+    return randomOutput
   }
+  if (data){
+    if(clickedSoundID){  //don't use array, use objects!!
+    data.forEach((item, index) => {
+      const soundSimilar = {
+        name: item.name,
+        id: item.id,
+        color: colorSVG[(randomColor(index))],
+        similar: []
+      }
+      freeSoundPreview(soundSimilar, clickedSoundID, clickedSoundIndex)
+      soundsGLOBAL[clickedSoundIndex].similar.push(soundSimilar)
+    })
+    //console.log(soundsGLOBAL[clickedSoundIndex].similar)
+  }else{
+    data.forEach((item, index) => {
+      const sound = {
+        name: item.name,
+        id: item.id,
+        color: colorSVG[index],
+        similar: []
+      }
+      freeSoundPreview(sound)
+      soundsGLOBAL.push(sound)
+    })
+  }
+}else{
+  $('#results').html('Zero Results')
 }
-
+}
 const handleFormSubmit = (event) =>{
   $('#results').html('')
   event.preventDefault()
   freeSoundAPI($('#q').val())
 }
 
-const createSVG = (data, index) =>{
+const createSVG = (data) =>{
 
-const svgCanvas = d3.select('main').html('')
-              .append('svg')
-                .attr('width', width)
-                .attr('height', height)
+  const svgCanvas = d3.select('main').html('')
+  .append('svg')
+  .attr('width', WIDTH)
+  .attr('height', HEIGHT)
 
-const circleAttributes = svgCanvas.selectAll('circle')
-                  .data(data)
-                  .enter()
-                  .append('circle')
-                      .attr("cx", function (d, i) {
-                        return (((i)%4)*200)+100})
-                      .attr("cy", function (d, i) {
-                        return (Math.floor(i/4)*200)+100})
-                      .attr("r", "40")
-                      .attr("id",  function (d,i) { return `button_${d.id}`; })
-                      .style("fill", function (d){return d.color})
-                      .on("mouseover", function(d) {
-                          d3.select(this).style("opacity", .2)
-                        //  d.previewMP3.loop('true', d.id) -- d.previewMP3.fade(0.0, 0.9, 100)
-                          d.previewMP3.play() //fade might not working!!
-                        })
-                      .on('mouseleave', function(d){
-                          d3.select(this).style("opacity", 1)
-                          //d.previewMP3.fade(.9, 0.0, 100) //fade might not working!!
-                          d.previewMP3.pause()
-                        })
-                      .on('click', function(d, i) {
-                          d.previewMP3.pause()
-                          freeSoundSimilar(d.id, i)
-                        })
-}
+  const circleAttributes = svgCanvas.selectAll('circle')
+  .data(data)
+  .enter()
+  .append('circle')
+  .attr("cx", function (d, i) {
+    return (((i)%4)*200)+100})
+    .attr("cy", function (d, i) {
+      return (Math.floor(i/4)*200)+100})
+      .attr("r", "40")
+      .attr("id",  function (d,i) { return `button_${d.id}`; })
+      .style("fill", function (d){return d.color})
+      .on("mouseover", function(d) {
+        d3.select(this).style("opacity", .2)
+        //  d.previewMP3.loop('true', d.id) -- d.previewMP3.fade(0.0, 0.9, 100)
+        d.previewMP3.play() //fade might not working!!
+      })
+      .on('mouseleave', function(d){
+        d3.select(this).style("opacity", 1)
+        //d.previewMP3.fade(.9, 0.0, 100) //fade might not working!!
+        d.previewMP3.pause()
+      })
+      .on('click', function(d, i) {
+        d.previewMP3.pause()
+        freeSoundSimilar(d.id, i)
+      })
+    }
 
-const setupUIHandlers = () => {
-   $('#FreeSoundSearch').on('submit', handleFormSubmit)
- }
-$(setupUIHandlers)
+    const setupUIHandlers = () => {
+      $('#FreeSoundSearch').on('submit', handleFormSubmit)
+    }
+    $(setupUIHandlers)
--- js/onClickSVG.js
+++ js/onClickSVG.js
@@ -1,94 +1,124 @@
 //onClickSVG
 const onClickSVG = (data, clickedSoundID) =>{
 
-    const radius = 200
-    const numNodes = 8
-    const circleDestinationX = width/2
-    const circleDestinationY = height/2
-    console.log(soundsGLOBAL)
-    svgCanvas = d3.select('main')
+  const radius = 200
+  const numNodes = 8
+  const circleDestinationX = WIDTH/2
+  const circleDestinationY = HEIGHT/2
 
-    const circleSize = svgCanvas.selectAll('circle')
-                  .transition()
-                      .duration(500)
-                      .attr("r", 30)
+  const svgCanvas = d3.select('main').select('svg')
 
-    const circleMovement = svgCanvas.select(`#button_${clickedSoundID}`)
-                  .transition()
-                      .duration(500)
-                      .attr("cx", circleDestinationX)
-                      .attr("cy", circleDestinationY)
+  svgCanvas.selectAll('circle')
+  .transition()
+  .duration(500)
+  .attr("r", 30)
 
-    const createNodes = function (numNodes, radius) {
-         let nodes = [],
-            angle,
-            x,
-            y,
-            i
-         for (i=0; i<numNodes; i++) {
-          angle = (i / (numNodes/2)) * Math.PI // Calculate the angle at which the element will be placed.
-                                                // For a semicircle, we would use (i / numNodes) * Math.PI.
-          x = (radius * Math.cos(angle)) + (width/2) // Calculate the x position of the element.
-          y = (radius * Math.sin(angle)) + (width/2) // Calculate the y position of the element.
-          nodes.push({'id': i, 'w': width, 'h': width, 'x': x, 'y': y})
-         }
-         console.log(nodes)
-         return nodes
-       }
+  svgCanvas.select(`#button_${clickedSoundID}`)
+  .transition()
+  .duration(500)
+  .attr("cx", circleDestinationX)
+  .attr("cy", circleDestinationY)
 
-const createElements = function (svgCanvas, nodes, elementRadius) {
+  .on('end', function(){
+    let nodes = createNodes(numNodes, radius, data);
+    createElements(svgCanvas, nodes, 25, data, clickedSoundID);
+  })
+}
+const createNodes = function (numNodes, radius, data) {
+  let nodes = [],
+  angle,
+  x,
+  y,
+  i
 
- let lines = svgCanvas.selectAll('line')
-                   .data(nodes)
-                   .enter()
-                   .append('line')
-                       .style("stroke", "black")  // colour the line
-                       .attr("x1", function (d, i) {
-                         return d.w/2})
-                       .attr("y1", function (d, i) {
-                         return d.h/2})
-                       .attr("x2", function (d, i) {
-                           return d.w/2})
-                       .attr("y2", function (d, i) {
-                           return d.h/2})
-                   .transition()
-                   .duration(2000)
-                       .attr("x2", function (d, i) {
-                         return d.x })
-                       .attr("y2", function (d, i) {
-                         return d.y })
+  for (i=0; i<numNodes; i++) {
+    angle = (i / (numNodes/2)) * Math.PI // Calculate the angle at which the element will be placed.
+    // For a semicircle, we would use (i / numNodes) * Math.PI.
+    x = (radius * Math.cos(angle)) + (WIDTH/2) // Calculate the x position of the element.
+    y = (radius * Math.sin(angle)) + (HEIGHT/2) // Calculate the y position of the element.
 
- let element = svgCanvas.selectAll('circle')
-            .data(nodes)
-            .enter().append('circle')
-                .attr('r', 1)
-                .attr('cx', function (d, i) {
-                  return d.w/2;
-                })
-                .attr('cy', function (d, i) {
-                  return d.h/2;
-                })
+    nodes.push({'id': i, 'w': WIDTH, 'h': HEIGHT, 'x': x, 'y': y, 'id':data[i].id, 'color':data[i].color ,'url': data[i].previewMP3 }, ) //'url': urls[i]})
+  }
+  return nodes
+}
+
+const createElements = function (svgCanvas, nodes, elementRadius, data, clickedSoundID) {
+  console.log(nodes)
+  // svgCanvas.selectAll('line')
+  //         .data(data)
+  //         .append('line')
+  //             .style("stroke", "blue") //function (d){return d.color})
+
+  svgCanvas.selectAll('line')
+  .data(nodes)
+  .enter()
+  .append('line')  // colour the line
+  .style("stroke", function (d, i) {
+    return d.color})
+    .attr("x1", function (d, i) {
+      return d.w/2})
+      .attr("y1", function (d, i) {
+        return d.h/2})
+        .attr("x2", function (d, i) {
+          return d.w/2})
+          .attr("y2", function (d, i) {
+            return d.h/2})
             .transition()
             .duration(2000)
-                .attr('r', elementRadius)
-                .attr('cx', function (d, i) {
-                  return d.x;
-                })
-                .attr('cy', function (d, i) {
-                  return d.y;
-                })
-console.log('element')
-}
- const draw = function () {
-   let nodes = createNodes(numNodes, radius);
-   // createSvg(radius, function (svg)
-   createElements(svgCanvas, nodes, 25);
+            .attr("x2", function (d, i) {
+              return d.x })
+              .attr("y2", function (d, i) {
+                return d.y })
 
- }
+svgCanvas.selectAll('circles') //(`#button_${clickedSoundID}`)
+          .data(nodes)
+          .enter()
+          .append('circle')
+              .attr('r', 1)
+              .attr('cx', function (d, i) {
+                return WIDTH/2;
+              })
+              .attr('cy', function (d, i) {
+                return HEIGHT/2;
+              })
+              .attr("id",  function (d,i) { return `button_${d.id}`; })
+              .style("fill", function (d, i) {
+                return d.color}) //function (d){return d.color})
+              .on("mouseover", function(d) {
+                d3.select(this).style("opacity", .2)
+                //  d.previewMP3.loop('true', d.id) -- d.previewMP3.fade(0.0, 0.9, 100)
+                d.url.play() //fade might not working!!
+                console.log(d.url.play())
+              })
+              .on('mouseleave', function(d){
+                d3.select(this).style("opacity", 1)
+                //d.previewMP3.fade(.9, 0.0, 100) //fade might not working!!
+                d.url.pause()
+              })
+              .on('click', function(d, i) {
+                d.url.pause()
+                freeSoundSimilar(d.id, i)
+              })
+              .transition(2000)
+                    .attr('r', elementRadius)
+                    .attr('cx', function (d, i) {
+                      return d.x;
+                    })
+                    .attr('cy', function (d, i) {
+                      return d.y;
+                    })
 
-$(document).ready(function() {
-    draw();
-    console.log('drawing')
-})
+        }
 
-}
+// svgCanvas.selectAll('circles')//(`#button_${clickedSoundID}`)
+//             .data(nodes)
+//             .transition()
+//             .duration(2000)
+//                 .attr('r', elementRadius)
+//                 .attr('cx', function (d, i) {
+//                   return d.x;
+//                 })
+//                 .attr('cy', function (d, i) {
+//                   return d.y;
+//                 })
+// }
